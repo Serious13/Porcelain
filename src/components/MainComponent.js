d0f290d45d1eb6192cd08ddf154f6ff0e7e93647
= <=>
2021-06-23 21:58:04 +0200

    Build 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -16,8 +16,8 @@ class Main extends Component {
                 <Switch>
                     <Route exact path = "/home" component = {Home}/>
                     <Redirect to="/home"/>
-                </Switch>                
-            </BrowserRouter>
+                </Switch>  
+            </BrowserRouter>                              
         )
     }
 }
